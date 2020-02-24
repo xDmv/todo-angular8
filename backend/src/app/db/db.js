171fe5c28079264552de6f6416cc7fd329188053
Daemon <makushin.dima@gmail.com>
2020-02-24 17:54:34 +0200

    up: backend

--- backend/src/app/db/db.js
+++ backend/src/app/db/db.js
@@ -1,54 +1,33 @@
-// module.exports = {
-// 	url : 'здесь будет ваш URL'
-// };
-module.exports.sqlite = require('sqlite-sync');
+const moment        = require('moment');
+const md5 = require('md5');
+const sqlite3 = require('sqlite3').verbose();
 
-module.exports.connectDB = () => {
-	return (exports.sqlite.connect('todos.db'));
-}
 
-module.exports.initializedDB = () => {
-	sqlite.run(`CREATE TABLE IF NOT EXISTS Notes(
+let db = new sqlite3.Database('notes.db', (err) => {
+	if (err) {
+		console.error(err.message)
+		throw err
+	} else {
+		console.log('Connected to the SQLite database.');
+		db.run(`CREATE TABLE IF NOT EXISTS Notes(
 			id INTEGER PRIMARY KEY AUTOINCREMENT, 
 			user_id INTEGER,
 			text TEXT,
-			complete BLOB,
-			important BLOB,
-			date_create INTEGER,
-			date_update INTEGER
-		);`
-	);
-	sqlite.run(`CREATE TABLE IF NOT EXISTS Users(
-			id INTEGER PRIMARY KEY AUTOINCREMENT, 
-			name TEXT,
-			email TEXT,
-			password TEXT,
-			role_id INTEGER,
-			date_create INTEGER,
-			date_update INTEGER
-		);`
-	);
-	sqlite.run(`CREATE TABLE IF NOT EXISTS Role(
-			id INTEGER PRIMARY KEY AUTOINCREMENT,
-			user_id INTEGER, 
-			role TEXT,
-			comment TEXT,
-			date_create INTEGER,
-			date_update INTEGER
-		);`
-	);
-}
+			complete INTEGER,
+			important INTEGER,
+			date_create TEXT,
+			date_update TEXT
+		);`);
+		(err) => {
+            if (err) {
+                // Table already created
+            }else{
+				let create_date = moment().format('dd.mm.yyyy HH:mm:ss');
+				let insertNote = `INSERT INTO Notes (user_id, text, complete, important, date_create, date_update) VALUES ( 911, 'Testing data', 0, 0, ${create_date}, ${create_date})`;
+				db.run(insertNote);
+			}
+		}
+	}
+});
 
-module.exports.addTodo = (
-	user_id, text, complete, important, date_create, date_update
-	) => {
-		const rezult =	sqlite.insert(Notes, {
-			user_id: user_id,
-			text: text,
-			complete: complete,
-			important: important,
-			date_create: date_create,
-			date_update: date_update
-		});
-		return (rezult);
-}
+module.exports = db;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,51 +1,121 @@
-const express          = require('express');
-// const MongoClient = require('mongodb').MongoClient;
+const express         = require('express');
 const bodyParser   = require('body-parser');
+const moment        = require('moment');
 const app                = express();
-const http = require("http");
-const db = require('../src/app/db/db');
-const router = express.Router();
-// require('./app/routes')(app, {});
+const db                  = require('../src/app/db/db');
 
-const port = 8000;
+const router           = express.Router();
+
+const port = 4250;
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+
+// function addNote(
+// 	user_id, text, complete, important, date_create, date_update
+// ){
+// 	let sql = `INSERT INTO Notes VALUES (
+// 		user_id: ${user_id},
+// 		text: ${text},
+// 		complete: ${complete},
+// 		important: ${important},
+// 		date_create: ${date_create},
+// 		date_update: ${date_update}
+// 	)`
+// 	let rezult_sql = db.prepare(sql);
+// }
+
 app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: true }));
 
-// MongoClient.connect(db.url, (err, database) => {
-// 	if (err) return console.log(err)
-// 	require('./app/routes')(app, database);
+app.get("/", function (request, response) {
+	console.log(request.body);
+	console.log('get api test');
+	response.status(200).json({data: `Not found!`, status: false});
+	// response.sendStatus(404);
+});
 
-// 	app.listen(port, () => {
-// 		console.log('We are live on ' + port);
-// 	});
-// })
+app.get("/api", function (request, response) {
+	// console.log(request.body);
+	console.log('get api test');
+	let sql = "select * from Notes;";
+	let param = [];
+	db.all(sql, param, (err, rows) => {
+		if (err) {
+			response.status(400).json({"error":err.message});
+			return;
+		}
+		response.json({
+			"message":"success",
+			"data": rows
+		})
+	});
+	// response.status(200).json({data: 'not found'});
+	// response.sendStatus(404);
+});
 
-// app.get("/api", function (request, response) {
-// 	console.log(request.body);
-// 	console.log('get api test');
-// 	response.status(404).send(`Ресурс не найден!`);
-// 	// response.sendStatus(404);
-// });
+app.get("/api/:id", function (request, response) {
+	console.log(request.body);
+	console.log('get api test');
+
+	response.status(200).json({data: 'not found id'});
+	// response.sendStatus(404);
+});
 
 app.post("/api/post", urlencodedParser, function (request, response) {
-	console.log('post')
-	if (!request.body.userName) return (response.status(404).json({error: "Invalid POST"}) );
-    console.log(request.body);
-    response.send(`${request.body.userName} - ${request.body.userAge}`);
-});
-
-router.post('/api/post1', async (req, res, next) => {
-	const body = req.body;
+	const body = request.body;
 	try {
-		console.log(` Name: ${body.userName} `);
-		return res.status(201).json({ complete: 'done' });
+		if(body.text){
+			let user_id = 666;
+			
+			console.log(body.text);
+			console.log(body.done);
+			let done = 0;
+			let important = 0;
+			if(body.done === true){ done = 1}
+			if(body.important === true){ done = 1}
+			console.log(`done: ${done} important: ${important}`);
+			let create_date = moment().format('dd.mm.yyyy HH:mm:ss');
+			let sql = `INSERT INTO Notes (user_id, text, complete, important, date_create, date_update) VALUES ( ${user_id}, ${body.text}, 0, 0, ${create_date}, ${create_date})`;
+			let param = [];
+			db.run(sql, param, (err, rows) => {
+				if (err) {
+					response.status(400).json({"error":err.message});
+					return;
+				}
+				response.json({
+					"message":"success",
+					"data": rows
+				})
+			});
+			// console.log(result);
+			// return response.status(201).json({ status: 'success', message: 'save this data', data: body });
+		}
+		response.status(401).json({ status: 'error', message: 'have not data'  });
 	}
 	catch (err) { 
-		return res.status(400).json({ error: err.message });
+		return response.status(400).json({ error: err.message });
 	}
 });
 
+app.put("/api/put/:id", urlencodedParser, function (request, response){
+	const body = request.body;
+	console.log(`put id: ${request.params.id}`)
+	if(body.text){
+		return response.status(201).json({ status: 'success', message: 'save this data', data: body});
+	}
+	response.status(401).json({ status: 'error', message: 'have not data'  });
+});
+
+app.delete('api/delete/:id', urlencodedParser, (request, response) => {
+	//	const body = request.body;
+	//	console.log(`body: ${body}`);
+	console.log(`delete id: ${request.params.id}`);
+	// try {
+	// 	return response.status(200).json({ status: 'done', message: 'delete this data'});
+	// }
+	// catch (err) { 
+	// 	return response.status(400).json({ error: err.message });
+	// }
+});
 
 
-app.listen(4250);
+app.listen(port);
