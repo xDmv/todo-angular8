4188e2484de78271e2908eb8c9d174918d5523de
Daemon <makushin.dima@gmail.com>
2020-02-24 17:54:02 +0200

    up: frontend

--- frontend/src/app/services/api.service.spec.ts
+++ frontend/src/app/services/api.service.spec.ts
@@ -2,15 +2,25 @@ import { TestBed } from '@angular/core/testing';
 
 import { ApiService } from './api.service';
 
-describe('ApiService', () => {
-  let service: ApiService;
+// describe('ApiService', () => {
+//   let service: ApiService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ApiService);
-  });
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({});
+//     const service: TestService = TestBed.get(TestService);
+//     service = TestBed.inject(ApiService);
+//   });
+
+//   it('should be created', () => {
+//     expect(service).toBeTruthy();
+//   });
+// });
+
+describe('ApiService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
+    const service: ApiService = TestBed.get(ApiService);
     expect(service).toBeTruthy();
   });
 });
